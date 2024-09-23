4dfc8ce0fa2c2801c05fdba1783b7932b4d52b40
AkashKumar Yadav <ay355231@gmail.com>
2024-09-23 23:26:22 +0530

    Completed KeyPress Event

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -30,3 +30,15 @@ for (let i = 0; i < btnShowModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+
+    console.log(e.key);
+
+    if (e.key === "Escape" && !modal.classList.contains('hidden')) {
+
+        closeModal();
+
+    }
+
+})
